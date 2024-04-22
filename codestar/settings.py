082defd4b46425fc7bd344e13b37d02565c09d77
Gary Sorohan <garysorohan@gmail.com>
2024-04-22 11:51:15 +0000

    add a Comment model class

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-soro82-djangoblog-nxkse84ikcv.ws-eu110.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-soro82-djangoblog-nxkse84ikcv.ws-us110.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -90,7 +90,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.codeanyapp.com",
+    "https://*.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
